ddccb5c9f8333356b45180fd74a00efd02d4e107
Nicolas Piker <80845762+nicop87@users.noreply.github.com>
2025-04-22 15:42:38 -0700

    Added previous TCP assignment
    added from my repo from last assignment

---
+++ client.py
@@ -0,0 +1,44 @@
+import ipaddress
+import socket 
+
+
+if __name__ == "__main__":
+    # recieves input from client which contains the IP address, port number of the server, and a message to send to that server
+    target_IP = input("Please enter the target ip address: ")
+    target_port = int(input("Please enter the target port number of the server: "))
+    message = input("What is your message? ")
+
+    # creates the socket for our communication
+    myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    #attempts to connect to the server
+    while True:
+        try:
+            myTCPSocket.connect((target_IP, target_port))
+            break
+
+        #if there is an error with connecting
+        except:
+            print("An error has occurred please try again")
+            target_IP = input("Please enter the target ip address: ")
+            target_port = int(input("Please enter the target port number of the server: "))
+            message = input("What is your message? ")
+
+    #loop to send and recieve the messages 
+    while True:
+        #sends the message and encodes it
+        myTCPSocket.send(bytearray(str(message), encoding='utf-8'))
+        
+        #recieves and prints the response
+        response = myTCPSocket.recv(500).decode()
+        print("Returned message: ", response)
+
+        #checks to see if the client would want to continue
+        if input("Would you like to change your message? (Y/n)").lower() == 'y':
+            print()
+            message = input("What is your new message?")
+        else:
+            break
+
+    #closes the socket
+    myTCPSocket.close()
---
+++ server.py
@@ -0,0 +1,39 @@
+import ipaddress
+import socket 
+
+#creates a socket
+myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#asks for which port to connect to 
+target_IP = input("What ip address would you like to connect to? ")
+port = int(input("What port would you like to connect to? "))
+#connects the socket to a specific port on the computer
+myTCPSocket.bind((target_IP, port))
+
+#waits for a connection to be initiated 
+myTCPSocket.listen(5)
+
+#when it gets a connection it accepts it 
+incomingSocket, incomingAddress = myTCPSocket.accept()
+
+#continuously waiting for a message
+while True:
+
+    #gets the message, have to use decode to get rid of the 'b'
+    myData = str(incomingSocket.recv(500).decode())
+
+    #once the client ends it on their side it will send empty messages to the server
+    if myData == '':
+        break
+
+
+    print("UNCHANGED: ", myData)
+
+    #changes the message to all capital letters
+    myData = myData.upper()
+    print("ALTERED: ", myData)
+
+    #sends the message to the connected socket
+    incomingSocket.send(bytearray(str(myData), encoding='utf-8'))
+
+
